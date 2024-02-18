98dcabfd7f4945d3d1ad8b2de1d68f0deb54e973
Abubakkar72 <abubakkarsiddiqn697@gmail.com>
2024-02-18 21:21:21 +0530

    work done on bodyL, bodyE, bodyF, footer only for desktop

--- src/Components/desktop.js
+++ src/Components/desktop.js
@@ -47,6 +47,121 @@ const Desktop = () => {
 
                         </div>
                     </div> 
+
+                    {/* BodyL Section */}
+                    <div className="bodyG">
+                        <div className="rectangle2">
+                            <div className="frame16">
+                                <p>L O R E M</p>
+                                <span>Subtitle</span>
+                            </div>
+                        </div>
+
+                        <div className="frame17">
+                            <div className="rectangle3">
+                                <span className="number">01</span>
+                                <span className="title">L o r e m i p s u m d o l o r s i t</span>
+                                <span className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
+                            </div>
+                            <div className="rectangle4">
+                                <span className="number">02</span>
+                                <span className="title">L o r e m i p s u m d o l o r s i t</span>
+                                <span className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
+                            </div>
+                            <div className="rectangle5">
+                                <span className="number">03</span>
+                                <span className="title">L o r e m i p s u m d o l o r s i t</span>
+                                <span className="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</span>
+                            </div>
+                        </div>
+                        
+                    </div>
+
+                    {/* BodyE Section */}
+                    <div className="bodyE">
+                        <div className="rectangle6">
+                            <div className="rectangle7">
+
+                            </div>
+                        </div>
+
+                        <div className="frame23">
+                            <div className="frame21">
+                                <div className="frame20">
+                                    <span>Lorem ipsum</span>
+                                </div>
+                                <span>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed accumsan convallis
+                                    sem, nec consequat ligula efficitur in. Nullam sed justo felis. Ut euismod leo sit
+                                    amet ipsum molestie, at convallis eros sollicitudin. Donec non faucibus risus.
+                                    Vestibulum fermentum accumsan metus sit amet posuere.
+                                </span>
+                            </div>
+                            <div className="frame19">
+                                <span>Lorum ipsum</span>
+                                <div className="arrow">
+                                    <div className="vector">
+                                        {/* <svg width="52" height="53" viewBox="0 0 52 53" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <path d="M51.25 26.5C51.25 40.4452 39.9452 51.75 26 51.75C12.0548 51.75 0.75 40.4452 0.75 26.5C0.75 12.5548 12.0548 1.25 26 1.25C39.9452 1.25 51.25 12.5548 51.25 26.5Z" stroke="#6A45FF" stroke-width="1.5"/>
+                                        </svg> */}
+                                    </div>
+                                    <div className="eclipse">
+                                        <svg width="52" height="53" viewBox="0 0 52 53" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <g clip-path="url(#clip0_1070_258)">
+                                        <path d="M51.25 26.5C51.25 40.4452 39.9452 51.75 26 51.75C12.0548 51.75 0.75 40.4452 0.75 26.5C0.75 12.5548 12.0548 1.25 26 1.25C39.9452 1.25 51.25 12.5548 51.25 26.5Z" stroke="#6A45FF" stroke-width="1.5"/>
+                                        <path d="M16 26.5L36 26.5M36 26.5L28.5 34.5M36 26.5L28.5 18.5" stroke="#6A45FF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                                        </g>
+                                        <defs>
+                                        <clipPath id="clip0_1070_258">
+                                        <rect width="52" height="52" fill="white" transform="translate(52 0.5) rotate(90)"/>
+                                        </clipPath>
+                                        </defs>
+                                        </svg>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* BodyF Section */}
+                    <div className="bodyF">
+                        <div className="group">
+                            <div class="red"></div>
+                            <div class="blue"></div>
+                            <div class="green"></div>
+                            <div class="yellow"></div>
+                        </div>
+                    </div>
+
+                    {/* Footer Section */}
+                    <div className="footer">
+                        <div className="frame24">
+                            <span>Home</span>
+                            <span>Page1</span>
+                            <span>Page2</span>
+                        </div>
+
+                        <div className="media">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="#FFFFFF" class="bi bi-facebook" viewBox="0 0 16 16">
+                            <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951"/>
+                            </svg>
+
+                            <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="#FFFFFF" class="bi bi-twitter" viewBox="0 0 16 16">
+                            <path d="M5.026 15c6.038 0 9.341-5.003 9.341-9.334q.002-.211-.006-.422A6.7 6.7 0 0 0 16 3.542a6.7 6.7 0 0 1-1.889.518 3.3 3.3 0 0 0 1.447-1.817 6.5 6.5 0 0 1-2.087.793A3.286 3.286 0 0 0 7.875 6.03a9.32 9.32 0 0 1-6.767-3.429 3.29 3.29 0 0 0 1.018 4.382A3.3 3.3 0 0 1 .64 6.575v.045a3.29 3.29 0 0 0 2.632 3.218 3.2 3.2 0 0 1-.865.115 3 3 0 0 1-.614-.057 3.28 3.28 0 0 0 3.067 2.277A6.6 6.6 0 0 1 .78 13.58a6 6 0 0 1-.78-.045A9.34 9.34 0 0 0 5.026 15"/>
+                            </svg>
+
+                            <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="#FFFFFF" class="bi bi-instagram" viewBox="0 0 16 16">
+                            <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334"/>
+                            </svg>
+
+                            <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="#FFFFFF" class="bi bi-tiktok" viewBox="0 0 16 16">
+                            <path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3z"/>
+                            </svg>
+                        </div>
+
+                        <span>&copy; Logo, Inc.</span>
+                    </div>
                 </div>
             }
         </div>
